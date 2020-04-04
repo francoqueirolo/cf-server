179f0385d85f959a1f64f84c9f28166f161b2c9f
Franco Queirolo <fgqueirolo@gmail.com>
2020-04-04 01:26:01 -0500

    feat: register and login

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+//var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
-
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 
@@ -31,7 +29,6 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 //app.use(cookieParser('12345-67890-09876-54322'));
 
 app.use(session({
@@ -45,13 +42,13 @@ app.use(session({
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function auth (req, res, next) {
-    console.log(req.session);
+function auth(req, res, next) {
+  console.log(req.session);
 
-  if(!req.session.user) {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
+  if (!req.session.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
   }
   else {
     if (req.session.user === 'authenticated') {
@@ -69,8 +66,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
