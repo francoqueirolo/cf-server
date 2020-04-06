8c4273653989d2a1e37d12ce14a322357b7933f1
Franco Queirolo <fgqueirolo@gmail.com>
2020-04-06 00:45:55 -0500

    feat: Passport

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017/conFusion';
@@ -33,32 +35,28 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(session({
   name: 'session-id',
-  secret: '12345-67890-09876-54321',
+  secret: '12345-67890-09876-54322',
   saveUninitialized: false,
   resave: false,
   store: new FileStore()
 }))
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function auth(req, res, next) {
-  console.log(req.session);
+function auth (req, res, next) {
+  console.log(req.user);
 
-  if (!req.session.user) {
+  if (!req.user) {
     var err = new Error('You are not authenticated!');
     err.status = 403;
-    return next(err);
+    next(err);
   }
   else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
+        next();
   }
 }
 
