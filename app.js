46edeb3f941f67da79d9cd3de7a9edc9d6474be6
Franco Queirolo <fgqueirolo@gmail.com>
2020-04-12 15:20:38 -0500

    feat: HTTPS

--- app.js
+++ app.js
@@ -56,4 +56,14 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
+
 module.exports = app;
