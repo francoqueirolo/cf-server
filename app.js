ebea1be2b5a66b9ae5fdfae5f4da7290841ff23c
Franco Queirolo <fgqueirolo@gmail.com>
2020-04-12 15:23:18 -0500

    feat: HTTPS

--- app.js
+++ app.js
@@ -21,6 +21,16 @@ connect.then((db) => {
 }, (err) => { console.log(err); });
 
 var app = express();
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -54,16 +64,6 @@ app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
-
-// Secure traffic only
-app.all('*', (req, res, next) => {
-  if (req.secure) {
-    return next();
-  }
-  else {
-    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
-  }
 });
 
 module.exports = app;
