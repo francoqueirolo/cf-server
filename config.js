e8d8f075c327aacce0140f9812eb5c6ee8b32a14
Franco Queirolo <fgqueirolo@gmail.com>
2020-04-15 19:41:24 -0500

    feat: Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   'secretKey': '12345-67890-09876-54321',
-  'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+  'mongoUrl': 'mongodb://localhost:27017/conFusion',
+  'facebook': {
+      clientId: '452902922162717',
+      clientSecret: '8138bc28fe4a5823641f724bf04a8225'
+  }
 }
